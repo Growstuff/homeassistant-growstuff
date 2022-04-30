48b3722fc6fbe13950bb155a32ac6fae07bdd28e
Daniel O'Connor <daniel.oconnor@gmail.com>
2022-04-30 18:50:45 +0930

    Reformat with black

--- custom_components/growstuff/sensor.py
+++ custom_components/growstuff/sensor.py
@@ -4,27 +4,28 @@ import requests
 
 from homeassistant.helpers.entity import Entity
 
-_ICON = 'mdi:leaf'
+_ICON = "mdi:leaf"
 _API_URL = "https://www.growstuff.org/api/v1"
-_LOGGER = logging.getLogger('growstuff')
+_LOGGER = logging.getLogger("growstuff")
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up all plantings."""
-    member_url = '{api_url}/members?filter[login-name]={member}'.format(
-        api_url=_API_URL,
-        member=config.get('member'))
+    member_url = "{api_url}/members?filter[login-name]={member}".format(
+        api_url=_API_URL, member=config.get("member")
+    )
 
     member_result = requests.get(member_url).json().data()
 
     if len(member_result) == 0:
-       raise homeassistant.exceptions.ConfigEntryNotReady("Member not found, check configuration")
+        raise homeassistant.exceptions.ConfigEntryNotReady(
+            "Member not found, check configuration"
+        )
 
     member = member_result[0]
     plantings_url = "{api_url}/members/{member_id}/plantings{filters}".format(
-        api_url=_API_URL,
-        member_id=member.get('id'),
-        filters="?filter[finished]=false")
+        api_url=_API_URL, member_id=member.get("id"), filters="?filter[finished]=false"
+    )
 
     add_plantings(plantings_url, add_devices)
 
@@ -33,12 +34,12 @@ def add_plantings(plantings_url, add_devices):
     """Add plantings until we added them all."""
     response = requests.get(plantings_url).json()
     devices = []
-    for planting in (response.get('data')):
+    for planting in response.get("data"):
         devices.append(GrowstuffPlantingSensor(planting))
     add_devices(devices)
-    links = response.get('links')
-    if links.get('next'):
-        add_plantings(links.get('next'), add_devices)
+    links = response.get("links")
+    if links.get("next"):
+        add_plantings(links.get("next"), add_devices)
 
 
 class GrowstuffPlantingSensor(Entity):
@@ -46,10 +47,10 @@ class GrowstuffPlantingSensor(Entity):
 
     def __init__(self, planting):
         """Initialize the sensor."""
-        self.planting_id = planting.get('id')
-        self._links = planting.get('links')
-        self._attributes = planting.get('attributes')
-        self._relationships = planting.get('relationships')
+        self.planting_id = planting.get("id")
+        self._links = planting.get("links")
+        self._attributes = planting.get("attributes")
+        self._relationships = planting.get("relationships")
 
     # def update(self):
     #     """Fetch new state data for the sensor.
@@ -66,22 +67,22 @@ class GrowstuffPlantingSensor(Entity):
     @property
     def unique_id(self):
         """Return the ID of the sensor."""
-        return self._attributes.get('slug')
+        return self._attributes.get("slug")
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return 'growstuff_{name}'.format(name=self._attributes.get('slug'))
+        return "growstuff_{name}".format(name=self._attributes.get("slug"))
 
     @property
     def friendly_name(self):
         """The crop is used as the friendly name."""
-        return self._attributes.get('crop-name')
+        return self._attributes.get("crop-name")
 
     @property
     def state(self):
         """Return the state of the sensor."""
-        percent = self._attributes.get('percentage-grown')
+        percent = self._attributes.get("percentage-grown")
         if isinstance(percent, float):
             return round(percent, 2)
 
@@ -91,19 +92,19 @@ class GrowstuffPlantingSensor(Entity):
         return self._attributes
 
     def _url(self):
-        return self._links.get('self')
+        return self._links.get("self")
 
     @property
     def entity_picture(self):
         """Icon to use in the frontend, if any."""
-        return self._attributes.get('thumbnail')
+        return self._attributes.get("thumbnail")
 
     @property
     def unit_of_measurement(self):
         """Return the unit this state is expressed in."""
-        return '%'
+        return "%"
 
     def update(self):
         """Get the latest data from Growstuff and update the states."""
         response = requests.get(self._url())
-        self.__init__(response.json().get('data'))
+        self.__init__(response.json().get("data"))
