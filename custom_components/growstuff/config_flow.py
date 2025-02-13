77a8642e85271bdb317fd57bca6717f4c1fd9693
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-02-13 15:18:30 +0000

    Fix const

--- custom_components/growstuff/config_flow.py
+++ custom_components/growstuff/config_flow.py
@@ -61,7 +61,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     # This tells HA if it should be asking for updates, or it'll be notified of updates
     # automatically. This example uses PUSH, as the dummy hub will notify HA of
     # changes.
-    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLLING
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
