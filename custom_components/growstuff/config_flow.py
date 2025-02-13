61a745f94077cf1ffac02b5a2a1a8f65f0688184
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-02-13 15:21:59 +0000

    Validate user

--- custom_components/growstuff/config_flow.py
+++ custom_components/growstuff/config_flow.py
@@ -23,33 +23,9 @@ async def validate_input(hass: HomeAssistant, data: dict) -> dict[str, Any]:
     """
     # Validate the data can be used to set up a connection.
 
-    # if len(data["member"]) < 3:
-    #    raise InvalidHost
+    if len(data["member"]) < 3:
+       raise InvalidData
 
-    #     hub = Hub(hass, data["host"])
-    #     # The dummy hub provides a `test_connection` method to ensure it's working
-    #     # as expected
-    #     result = await hub.test_connection()
-    #     if not result:
-    #         # If there is an error, raise an exception to notify HA that there was a
-    #         # problem. The UI will also show there was a problem
-    #         raise CannotConnect
-
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data["username"], data["password"]
-    # )
-
-    # If you cannot connect:
-    # throw CannotConnect
-    # If the authentication is wrong:
-    # InvalidAuth
-
-    # Return info that you want to store in the config entry.
-    # "Title" is what is displayed to the user for this hub device
-    # It is stored internally in HA as part of the device config.
-    # See `async_step_user` below for how this is used
     return {"title": data["member"]}
 
 
@@ -77,8 +53,8 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 info = await validate_input(self.hass, user_input)
 
                 return self.async_create_entry(title=info["title"], data=user_input)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
+            except InvalidData:
+                errors["base"] = "invalid_data"
             except Exception:  # pylint: disable=broad-except
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
@@ -91,3 +67,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
 class CannotConnect(exceptions.HomeAssistantError):
     """Error to indicate we cannot connect."""
+
+class InvalidData(exceptions.HomeAssistantError):
+    """Error to indicate we bad data."""
+
